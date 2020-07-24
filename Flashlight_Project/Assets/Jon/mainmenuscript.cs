3a5d315e6acf3736c8468f8d0b52998be8392575
jonathanritson42 <jonathanritson42@gmail.com>
2020-07-24 18:40:49 +0100

    Initial menu set up

---
+++ Flashlight_Project/Assets/Jon/mainmenuscript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class mainmenuscript : MonoBehaviour//, IPointerEnterHandler
+{
+    public Button play, quit, settings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        play.onClick.AddListener(playgame);
+        quit.onClick.AddListener(quitprogram);
+        settings.onClick.AddListener(settingmenu);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void playgame()
+    {
+
+    }
+
+    void settingmenu()
+    {
+
+    }
+
+    void quitprogram()
+    {
+        Application.Quit();
+    }
+}
