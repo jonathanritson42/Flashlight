9b28f1a52ce7dc1ee8c5f6648c6de93d6b6a6eac
Sam Connolly <sam.connolly10@hotmail.co.uk>
2020-07-27 12:15:14 +0100

    Code push
    Object code

---
+++ Flashlight_Project/Assets/SamC/ObjectApperance.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectApperance : MonoBehaviour
+{
+    public List<GameObject> objects;
+
+    void CheckDistance()
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            if (Vector3.Distance(transform.position, objects[i].transform.position) < 10)
+            {
+                objects[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+
+    /*public GameObject objects;
+
+    public float distanceBetweenObjects;
+
+    private void Update()
+    {
+        distanceBetweenObjects = Vector3.Distance(transform.position, objects.transform.position);
+    }
+
+    void ObjectApperance()
+    {
+        if (distanceBetweenObjects <= 10)
+        {
+            objects.gameObject.SetActive(false);
+        }
+        else
+        {
+            objects.gameObject.SetActive(true);
+        }
+    }*/
+}
