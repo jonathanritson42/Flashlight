c3f448afbd65053aebfbb636ab42f13ec2e0d91f
jonathanritson42 <jonathanritson42@gmail.com>
2020-08-19 00:42:59 +0100

    Revert "Merge branch 'master' into jon"
    This reverts commit 6de9ac8324f287f5c92a535c231773cd66763292, reversing
    changes made to 470609a38305bcbd3e06a0a2525ea5a656405f85.

--- Flashlight_Project/Assets/Jon/code/flashlight.cs
+++ Flashlight_Project/Assets/Jon/code/flashlight.cs
@@ -5,10 +5,26 @@ using UnityEngine;
 public class flashlight : MonoBehaviour
 {
     public GameObject torchpoint;
+    public float delaySpeed;
+    private float dist;
+    public bool usedelay;
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = Vector3.Lerp(this.transform.position, torchpoint.transform.position, Time.deltaTime);
+        dist = Vector3.Distance(this.transform.position, torchpoint.transform.position);
+
+        if (usedelay)
+        {
+            // Based on set speed rather than delay
+            this.transform.position = Vector3.Lerp(this.transform.position, torchpoint.transform.position, Time.deltaTime * delaySpeed);
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, torchpoint.transform.rotation, Time.deltaTime * delaySpeed);
+        }
+        else
+        {
+            // Based on distance rather than a set speed of delay
+            this.transform.position = Vector3.Lerp(this.transform.position, torchpoint.transform.position, Time.deltaTime * (dist / 2));
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, torchpoint.transform.rotation, Time.deltaTime * (dist / 2));
+        }
     }
 }
