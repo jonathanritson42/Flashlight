c01df260f57ad4170650a78134e3330177201e0e
jonathanritson42 <jonathanritson42@gmail.com>
2020-08-27 14:15:19 +0100

    AI beginnings
    Ininial stages with the kinematics and AI movement

---
+++ Flashlight_Project/Assets/Jon/code/enemymoveleg.cs
@@ -0,0 +1,24 @@
+﻿using DitzelGames.FastIK;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemymoveleg : MonoBehaviour
+{
+    public GameObject[] legppoints, legtargets, legbonepoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < legbonepoints.Length; i++)
+        {
+            legtargets[i].transform.position = legppoints[i].transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
