0b49fd2dfcdb1805008437d96eb6df616711c032
Sam Connolly <sam.connolly10@hotmail.co.uk>
2020-09-03 15:51:53 +0100

    Portal and fixes
    Portal is working fully now. Changed the lamp so that Z and X are the activation keys. Task counter to allow you to move levels.

--- Flashlight_Project/Assets/SamC/working scripts/PortalActivation.cs
+++ Flashlight_Project/Assets/SamC/working scripts/PortalActivation.cs
@@ -9,7 +9,7 @@ public class PortalActivation : MonoBehaviour
     public GameObject CurrentLevelPortal;
 
     public int taskCounter = 0;
-    public int tasksToComplete = 2;
+    public static int tasksToComplete = 2;
 
 
     private void Start()
@@ -26,5 +26,19 @@ public class PortalActivation : MonoBehaviour
             taskCounter = 0;
             tasksToComplete += 1;
         }
+    }
+
+    public void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 10) && hit.transform.CompareTag("Objective"))
+            {
+                //play sound effect and animation
+                taskCounter++;
+            }
+        } 
     }
 }
--- Flashlight_Project/Assets/SamC/working scripts/portals/PortalTeleportsam.cs
+++ Flashlight_Project/Assets/SamC/working scripts/portals/PortalTeleportsam.cs
@@ -4,46 +4,49 @@ using UnityEngine;
 
 public class PortalTeleportsam : MonoBehaviour
 {
-    public Transform player;
+
+    public CharacterController player;
 
     public Transform receiver;
 
-    private bool playerIsOverlapping = false;
+     private bool playerIsOverlapping = false;
 
-    private void Update()
-    {
-        if (playerIsOverlapping)
-        {
-            Vector3 portalToPlayer = player.position - transform.position;
-            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+     private void Update()
+     {
+         if (playerIsOverlapping)
+         {
+             Vector3 portalToPlayer = player.transform.position - transform.position;
+             float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
 
-            if(dotProduct < 0f)
-            {
-                float rotationDifference = -Quaternion.Angle(transform.rotation, receiver.rotation);
-                rotationDifference += 180;
-                player.Rotate(Vector3.up, rotationDifference);
+             if(dotProduct < 0f)
+             {
+                 float rotationDifference = -Quaternion.Angle(transform.rotation, receiver.rotation);
+                 rotationDifference += 180;
+                 player.transform.Rotate(Vector3.up, rotationDifference);
 
-                Vector3 positionOffset = Quaternion.Euler(0f, rotationDifference, 0f) * portalToPlayer;
-                player.position = receiver.position + positionOffset;
-                playerIsOverlapping = false;
-            }
-        }
-    }
+                 Vector3 positionOffset = Quaternion.Euler(0f, rotationDifference, 0f) * portalToPlayer;
+                 player.enabled = false;
+                 player.transform.position = receiver.position + positionOffset;
+                 player.enabled = true;
+                 playerIsOverlapping = false;
+             }
+         }
+     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Player")
-        {
-            playerIsOverlapping = true;
-        }
-    }
+     private void OnTriggerEnter(Collider other)
+     {
+         if(other.tag == "Player")
+         {
+             playerIsOverlapping = true;
+         }
+     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.tag == "Player")
-        {
-            playerIsOverlapping = false;
-        }
-    }
+     private void OnTriggerExit(Collider other)
+     {
+         if(other.tag == "Player")
+         {
+             playerIsOverlapping = false;
+         }
+     }
 
 }
