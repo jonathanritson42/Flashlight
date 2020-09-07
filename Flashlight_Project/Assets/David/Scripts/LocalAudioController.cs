c96e60d8f6bcd70bb59f353a7d7028e76cc33429
David Barton <david.barton309@gmail.com>
2020-09-07 17:51:29 +0100

    Added Rain
    Added Rain Sound Effects and Visual Effects.
    Also added a system for changing reverb and sound effects based on what Room/area the player is in.

---
+++ Flashlight_Project/Assets/David/Scripts/LocalAudioController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalAudioController : MonoBehaviour
+{
+    
+    public GameObject HallwayReverb;
+    public GameObject KitchenReverb;
+    public GameObject DefaultReverb;
+    public int ReverbZoneActive = 0;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        KitchenReverb.SetActive(false);
+        HallwayReverb.SetActive(false);
+        DefaultReverb.SetActive(true);
+    }
+
+
+
+
+    public void SwitchReverb(int ZoneToActivate)
+    {
+        ReverbZoneActive = ZoneToActivate;
+        switch (ReverbZoneActive)
+        {
+            default:
+                break;
+            case 0:
+                KitchenReverb.SetActive(false);
+                HallwayReverb.SetActive(false);
+                DefaultReverb.SetActive(true);
+                break;
+            case 1:
+                HallwayReverb.SetActive(true);
+                KitchenReverb.SetActive(false);
+                DefaultReverb.SetActive(false);
+
+                break;
+            case 2:
+                HallwayReverb.SetActive(false);
+                KitchenReverb.SetActive(true);
+                DefaultReverb.SetActive(false);
+                break;
+
+            
+        } 
+        
+    }
+
+}
