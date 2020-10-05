70db8dfe2ae9f1ba6dfd6fc4f1f647360cbc2a30
jonathanritson42 <jonathanritson42@gmail.com>
2020-10-05 05:38:12 +0100

    voicelines and spider
    unfinished alturnate subtitles and audio player (done but with same bug as before)
    
    Spider collider and Wallscaling (needs new movement script as navmesh isn't great for this movement)

---
+++ Flashlight_Project/Assets/Jon/code/lamponoff.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lamponoff : MonoBehaviour
+{
+    public GameObject lampshade;
+    public Material replacemat;
+
+    public bool triggered;
+
+    private void Update()
+    {
+        if (triggered)
+        {
+            Trigger();
+        }
+    }
+
+    private void Trigger()
+    {
+        Light[] lights;
+
+        lights = GetComponentsInChildren<Light>();
+
+        foreach (Light lighting in lights)
+        {
+            lighting.enabled = false;
+        }
+
+        lampshade.GetComponent<MeshRenderer>().material = replacemat;
+    }
+}
